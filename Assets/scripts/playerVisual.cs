66473e0eb55b7e505fc1f014c2a056632ce3f9fe
Fenna03 <fenna.manders@sintlucasedu.nl>
2024-06-14 10:43:48 +0200

    IT WORKS!! YOU CAN SWITCH CHARACTER
    not to the level scene though but sshhhhh

--- Assets/scripts/playerVisual.cs
+++ Assets/scripts/playerVisual.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class playerVisual : MonoBehaviour
 {
-
     public void SetPlayerSkin(GameObject skin)
     {
+
+        foreach (Transform child in this.transform)
+        {
+            if (child.tag == "skin")
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
         Transform selectedSkin = this.gameObject.transform.Find(skin.name);
         selectedSkin.gameObject.SetActive(true);
     }
