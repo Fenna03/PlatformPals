a3d2c8c895f107830e233b1c098d3c7d94ce2396
Fenna03 <fenna.manders@sintlucasedu.nl>
2025-03-23 21:31:42 +0100

    players go to level 1
    level acts weird, because of online multiplayer, but skins go along great!

---
+++ Assets/scripts/Local/PlayerSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public PlayerInputManager InputManager;
+    public List<GameObject> skinPrefabs; // List of prefabs assigned in the Inspector
+    private GameObject instance;
+
+    private void Start()
+    {
+        SpawnPlayers();
+    }
+
+    private void SpawnPlayers()
+    {
+        // Loop through each player in LocalGameManager
+        for (int i = 0; i < LocalGameManager.Instance.localPlayerData.Count; i++)
+        {
+            LocalPlayerData data = LocalGameManager.Instance.localPlayerData[i];
+
+            // Get the correct prefab based on the skinId
+            GameObject prefab = skinPrefabs[data.skinId];
+
+            if (prefab == null)
+            {
+                Debug.LogError($"Prefab for skin ID {data.skinId} is missing!");
+                continue;
+            }
+
+            // Now, set the correct prefab dynamically in the InputManager
+            InputManager.playerPrefab = prefab;
+
+            // Instantiate the player prefab at the spawn position
+            instance = InputManager.JoinPlayer(data.playerIndex, data.splitscreen, null, data.device).gameObject;
+        }
+    }
+}
--- Assets/scripts/MovingPlayer.cs
+++ Assets/scripts/MovingPlayer.cs
@@ -95,7 +95,6 @@ public class MovingPlayer : NetworkBehaviour
         }
     }
 
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "ground" || collision.collider.tag == "Player" || collision.collider.tag == "button")
@@ -133,36 +132,3 @@ public class MovingPlayer : NetworkBehaviour
         }
     }
 }
-
-
-
-//void Moving()
-//{
-//    if (Input.GetKey(KeyCode.A))
-//    {
-//        transform.Translate(Vector3.left * speed * Time.deltaTime);
-//        horizontal = -1f;
-//        anim.SetBool("isRunning", true);
-//    }
-//    else if (Input.GetKey(KeyCode.D))
-//    {
-//        transform.Translate(Vector3.right * speed * Time.deltaTime);
-//        horizontal = 1f;
-//        anim.SetBool("isRunning", true);
-//    }
-//    else
-//    {
-//        anim.SetBool("isRunning", false);
-//    }
-//    if (Input.GetKey(KeyCode.W) && jumpAmount < 1 || Input.GetKey(KeyCode.Space) && jumpAmount < 1)
-//    {
-//        rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-//        jumpAmount++;
-//        isGrounded = false;
-//        anim.SetBool("isJumping", true);
-//        if (jumpAmount >= 1)
-//        {
-//            anim.SetBool("isFalling", true);
-//        }
-//    }
-//}
